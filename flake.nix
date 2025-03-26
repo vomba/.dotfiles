5b235458b789aa216701bb4ae63336737e4d0e46
vomba <hani.harzallah@elastisys.com>
2025-03-26 14:43:59 +0100

    add

--- flake.nix
+++ flake.nix
@@ -4,22 +4,33 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
-            url = "github:nix-community/home-manager";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-  };
-
-  outputs = inputs: {
-    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
- 
-    homeConfigurations = {
-	    # TODO: Modify "your.username" below to match your username
-      "vomba" = inputs.home-manager.lib.homeManagerConfiguration {
-        system = "x86_64-linux"; # TODO: replace with x86_64-linux on Linux
-        homeDirectory = "/home/hani"; # TODO: make this match your home directory
-        username = "vomba"; # TODO: Change to your username
-        configuration.imports = [ ./home.nix ];
-      };
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs =
+    inputs@{ self, nixpkgs, ... }:
+    {
+
+      homeConfigurations = {
+        # TODO: Modify "your.username" below to match your username
+        "hani" = inputs.home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          modules = [
+            ./home.nix
+            {
+              home = {
+                username = "hani";
+                homeDirectory = "/home/hani";
+                stateVersion = "25.05";
+              };
+            }
+          ];
+        };
+      };
+
+      hani = self.homeConfigurations.hani.activationPackage;
+      defaultPackage.x86_64-linux = self.hani;
+    };
 }
--- home.nix
+++ home.nix
@@ -1,3 +1,20 @@
-{ ... }: {
+{ pkgs, ... }:
+{
   programs.home-manager.enable = true;
+  # programs.git.enable = true;
+  # programs.git.includes = [
+  #   { path = ".gitconfig"; }
+  # ];
+
+  home.file.".\\gitconfig".source = ./gitconfig;
+
+  home.packages = with pkgs; [
+    pkgs.cowsay
+    pkgs.direnv
+    pkgs.zoxide
+    pkgs.git
+    pkgs.nixfmt-tree
+    pkgs.nil
+  ];
+
 }
