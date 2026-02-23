8c59adc1506cf83202539c564fe5cfb306dfb8e7
vomba <hani.harzallah@elastisys.com>
2026-02-23 16:28:23 +0100

    disable lmstudio

--- modules/ai.nix
+++ modules/ai.nix
@@ -4,33 +4,6 @@
     enable = if pkgs.stdenv.isLinux then false else true;
     settings = {
       provider = {
-        mistral = {
-          npm = "@ai-sdk/openai-compatible";
-          name = "Mistral AI";
-          options = {
-            baseURL = "https://api.mistral.ai/v1";
-          };
-        };
-        google = {
-          models = {
-            "gemini-2.5-pro" = { };
-          };
-        };
-        lmstudio = {
-          npm = "@ai-sdk/openai-compatible";
-          name = "LM Studio (local)";
-          options = {
-            baseURL = "http://localhost:1234/v1";
-          };
-          models = {
-            "jan-v3-4b-base-instruct" = {
-              name = "jan";
-            };
-            "ministral-3-3b-reasoning-2512" = {
-              name = "ministral 3 reason";
-            };
-          };
-        };
       };
     };
   };
@@ -40,7 +13,7 @@
     ++ (
       if pkgs.stdenv.isDarwin then
         [
-          pkgs-stable.lmstudio
+          # pkgs-stable.lmstudio
         ]
       else
         [ ]
