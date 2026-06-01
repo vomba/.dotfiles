d8ce5a7ccd61652cdec0ba77008fbbc30445a929
vomba <hani.harzallah@elastisys.com>
2026-06-01 11:54:04 +0200

    fix: pin sops to v3.10.0

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,4 +9,5 @@ languages self super
   openstack-tui = super.callPackage ./openstack-tui.nix { };
   kubernetes-helm = import ./helm.nix { inherit super; };
   helmfile = import ./helmfile.nix { inherit super; };
+  sops = import ./sops.nix { inherit super; };
 }
